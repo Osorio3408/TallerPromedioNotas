da478217208cc204337c0fac88ea733d444bbd4b
Osorio3408 <yuliamwow@gmail.com>
2023-03-22 17:51:46 -0500

    actualizacion

--- app/src/main/java/com/osorio/actividad_notas/MainActivity.kt
+++ app/src/main/java/com/osorio/actividad_notas/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.osorio.actividad_notas
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,26 +18,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculate() {
-        val inputStudentName:EditText=findViewById(R.id.student_name)
-        val studentName:String = inputStudentName.text.toString()
 
-        val inputSubject:EditText=findViewById(R.id.subject)
-        val Subject:String = inputSubject.text.toString()
+        val inputStudentName: EditText = findViewById(R.id.student_name)
+        val studentName: String = inputStudentName.text.toString()
 
-        val inputNote1:EditText=findViewById(R.id.note_1)
-        val note1:Double = inputNote1.text.toString().toDouble()
+        val inputSubject: EditText = findViewById(R.id.subject)
+        val subject: String = inputSubject.text.toString()
 
-        val inputNote2:EditText=findViewById(R.id.note_2)
-        val note2:Double = inputNote2.text.toString().toDouble()
+        val inputNote1: EditText = findViewById(R.id.note_1)
+        val note1: Double = inputNote1.text.toString().toDouble()
 
-        val inputNote3:EditText=findViewById(R.id.note_3)
-        val note3:Double = inputNote3.text.toString().toDouble()
+        val inputNote2: EditText = findViewById(R.id.note_2)
+        val note2: Double = inputNote2.text.toString().toDouble()
 
-        var Result:Double = (note1 + note2 + note3)/3;
+        val inputNote3: EditText = findViewById(R.id.note_3)
+        val note3: Double = inputNote3.text.toString().toDouble()
 
+        val average: Double = (note1 + note2 + note3) / 3
 
+        val message = "Bienvenido a $subject, $studentName! Promedio: $average"
 
-        Toast.makeText(this, "Bienvenido a $studentName", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, message,Toast.LENGTH_LONG).show()
+
+        Log.d("PEPE", message);
 
     }
 
