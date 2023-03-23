08f9915beaa430c2593f86d718a82fe09db5f505
Osorio3408 <yuliamwow@gmail.com>
2023-03-23 14:37:37 -0500

    se termino de realizar el programa

--- app/src/main/java/com/osorio/actividad_notas/MainActivity.kt
+++ app/src/main/java/com/osorio/actividad_notas/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.osorio.actividad_notas
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,30 +18,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculate() {
-
         val inputStudentName: EditText = findViewById(R.id.student_name)
-        val studentName: String = inputStudentName.text.toString()
+
 
         val inputSubject: EditText = findViewById(R.id.subject)
-        val subject: String = inputSubject.text.toString()
+
 
         val inputNote1: EditText = findViewById(R.id.note_1)
-        val note1: Double = inputNote1.text.toString().toDouble()
+
 
         val inputNote2: EditText = findViewById(R.id.note_2)
-        val note2: Double = inputNote2.text.toString().toDouble()
+
 
         val inputNote3: EditText = findViewById(R.id.note_3)
-        val note3: Double = inputNote3.text.toString().toDouble()
 
-        val average: Double = (note1 + note2 + note3) / 3
 
-        val message = "Bienvenido a $subject, $studentName! Promedio: $average"
-
-        Toast.makeText(this, message,Toast.LENGTH_LONG).show()
-
-        Log.d("PEPE", message);
-
+     val intent = Intent(this,ResultadoPromedio::class.java)
+     val miBundle:Bundle=Bundle()
+        miBundle.putString("NameStudent",inputStudentName!!.text.toString())
+        miBundle.putString("Subject",inputSubject.text.toString())
+        miBundle.putDouble("Note1", inputNote1.text.toString().toDouble())
+        miBundle.putDouble("Note2", inputNote2.text.toString().toDouble())
+        miBundle.putDouble("Note3", inputNote3.text.toString().toDouble())
+        intent.putExtras(miBundle)
+        startActivity(intent)
     }
 
 
--- app/src/main/java/com/osorio/actividad_notas/ResultadoPromedio.kt
+++ app/src/main/java/com/osorio/actividad_notas/ResultadoPromedio.kt
@@ -2,10 +2,56 @@ package com.osorio.actividad_notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultadoPromedio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado_promedio)
+
+        val Name_Student = findViewById<TextView>(R.id.Result_Name)
+        val Subject_Student = findViewById<TextView>(R.id.Result_Subject)
+        val Note1_Student = findViewById<TextView>(R.id.Result_Note1)
+        val Note2_Student = findViewById<TextView>(R.id.Result_Note2)
+        val Note3_Student = findViewById<TextView>(R.id.Result_Note3)
+        val Promedio_Student = findViewById<TextView>(R.id.Result_Promedio)
+
+        var miBundle:Bundle? = this.intent.extras
+
+        if(miBundle != null){
+            Name_Student.text="${miBundle.getString("NameStudent")}"
+            Subject_Student.text="${miBundle.getString("Subject")}"
+            val Note1:Double = miBundle.getDouble("Note1")
+            val Note2:Double = miBundle.getDouble("Note2")
+            val Note3:Double = miBundle.getDouble("Note3")
+            val NameStundent:String? = miBundle.getString("NameStudent")
+            Note1_Student.text="${miBundle.getDouble("Note1")}"
+            Note2_Student.text="${miBundle.getDouble("Note2")}"
+            Note3_Student.text="${miBundle.getDouble("Note3")}"
+
+            var resultPromedio:Double = (Note1 + Note2 + Note3)/3;
+
+            val result = if (resultPromedio>=3.5){
+                Promedio_Student.setTextColor(resources.getColor(R.color.green))
+                "El estudiante ${NameStundent} Aprobó"
+            }  else {
+                Promedio_Student.setTextColor(resources.getColor(R.color.red))
+                "El estudiante ${NameStundent} Reaprobó"
+            }
+
+            Promedio_Student.text="El resultado es: ${resultPromedio} y ${result}"
+
+
+        }
+
+        val btnSalir:Button = findViewById(R.id.Exit)
+        btnSalir.setOnClickListener { onClick() }
+
+
+    }
+
+    private fun onClick() {
+        finish()
     }
 }
